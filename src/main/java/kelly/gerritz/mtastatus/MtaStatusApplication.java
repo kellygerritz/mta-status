edf9d4076612a94b5498291c02d23f7235112d6b
Kelly Gerritz <kelly.gerritz@gmail.com>
2020-10-03 10:39:20 -0400

    mta initialize commit

---
+++ src/main/java/kelly/gerritz/mtastatus/MtaStatusApplication.java
@@ -0,0 +1,13 @@
+package kelly.gerritz.mtastatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtaStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtaStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kelly/gerritz/mtastatus/MtaStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package kelly.gerritz.mtastatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtaStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
